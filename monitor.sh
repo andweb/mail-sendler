b4364da26439796312ba31b8d173b3b16834ea9f
Andrei Hramov <andrei.hramov@simbirsoft.com>
2019-02-01 12:04:32 +0400

    fixed monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,5 +3,5 @@
 mkdir ./code/public/monitor
 git clone https://github.com/yugene/Gearman-Monitor.git ./code/public/monitor
 docker-compose exec --user=www-data php composer install -d /var/www/mail-sendler/public/monitor
-docker cp "./docker/gearman/monitor/_config.php.example" (docker-compose ps -q php):/var/www/mail-sendler/public/monitor/_config.php
+docker cp "./docker/gearman/monitor/_config.php.example" $(docker-compose ps -q php):/var/www/mail-sendler/public/monitor/_config.php
 
